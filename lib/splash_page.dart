f609ed50c833f7f9c9af8aac8a7b59ab44e89805
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-11 12:35:59 -0300

    adicionando splash page e atualizando pubspec.yaml

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -8,6 +9,37 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color(0xFF4054B2),
+              Color(0xFF6EC1E4),
+            ])),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height - 100,
+              child: Image.asset("asset/grupfy-logo.png"),
+            ),
+            SpinKitThreeBounce(
+              color: Colors.white,
+              size: 35,
+            ),
+            Text(
+              "Teste Fluter + API",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
