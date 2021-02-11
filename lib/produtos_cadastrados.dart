2af40d67ffab2fe9b863552d584efa3093a6993f
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-11 16:55:05 -0300

    nome do arquivo produtos_cadastrados.dart renomeado

---
+++ lib/produtos_cadastrados.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProdutosCadastrados extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Produtos"),
+      ),
+      body: Container(),
+    );
+  }
+}
