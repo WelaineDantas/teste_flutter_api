914e9a87d9ede58225fc201be6a0fac3e6c370cd
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-12 17:04:08 -0300

    adicionando widgets a tela produtos_cadastrados.

--- lib/produtos_cadastrados.dart
+++ lib/produtos_cadastrados.dart
@@ -29,10 +29,59 @@ class _ProdutosCadastradosState extends State<ProdutosCadastrados> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Produtos"),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: Text("Produtos"),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            Text("Lista de Produtos cadastrados:"),
+            SizedBox(height: 20),
+            Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 3,
+                  ),
+                ),
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: controllerListaProdutos.stream,
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Text("Carregando...");
+                        break;
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        if (snapshot.hasError) {
+                          return Text("Erro ao carregar lista de produtos!");
+                        } else {
+                          QuerySnapshot querySnapshot = snapshot.data;
+                          if (querySnapshot.docs.length == 0) {
+                            return Text("Nenhum produto foi cadastrado.");
+                          } else {
+                            return ListView.separated(
+                              itemCount: querySnapshot.docs.length,
+                              separatorBuilder: (context, index) => Divider(
+                                height: 2,
+                              ),
+                              itemBuilder: (context, index) {
+                                List<DocumentSnapshot> produtos =
+                                    querySnapshot.docs.toList();
+                                DocumentSnapshot item = produtos[index];
+
+                                return ListTile();
+                              },
+                            );
+                          }
+                        }
+                        break;
+                      default:
+                    }
+                  },
+                ))
+          ],
+        )));
   }
 }
