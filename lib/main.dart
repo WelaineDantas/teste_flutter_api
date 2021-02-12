95a3230706b65682e5fa5de86d228b5d6007fedb
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-12 14:58:18 -0300

    incluindo Firebase.initializeApp no main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:teste_flutter_api/route.dart';
 import 'package:teste_flutter_api/splash_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
