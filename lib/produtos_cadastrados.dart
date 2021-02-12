69a62ccb5129cb09edc1ee3491fc1d3345e46d67
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-12 16:36:47 -0300

    adicionando metodo adicionarListenerProdutos no arquivo produtos_cadastrados.dart

--- lib/produtos_cadastrados.dart
+++ lib/produtos_cadastrados.dart
@@ -1,6 +1,31 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class ProdutosCadastrados extends StatelessWidget {
+class ProdutosCadastrados extends StatefulWidget {
+  @override
+  _ProdutosCadastradosState createState() => _ProdutosCadastradosState();
+}
+
+class _ProdutosCadastradosState extends State<ProdutosCadastrados> {
+  final controllerListaProdutos = StreamController<QuerySnapshot>.broadcast();
+
+  Stream<QuerySnapshot> adicionarListenerProdutos() {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    final stream = db.collection("produtos").snapshots();
+
+    stream.listen((dados) {
+      controllerListaProdutos.add(dados);
+    });
+  }
+
+  @override
+  void initState() {
+    adicionarListenerProdutos();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
