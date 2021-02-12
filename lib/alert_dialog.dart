fc16781561d130c587abb56bda29db42bf4569fe
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-12 14:54:13 -0300

    adicionando metodo adicionarProduto

--- lib/alert_dialog.dart
+++ lib/alert_dialog.dart
@@ -1,9 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:teste_flutter_api/produto.dart';
 
 class AlertDialogCustomizado extends StatelessWidget {
   var controllerNome = TextEditingController();
   var controllerQuantidade = TextEditingController();
   var controllerValidade = TextEditingController();
+
+  adicionarProduto(String nome, String quantidade, String validade) {
+    Produto produto = Produto();
+    produto.nome = nome;
+    produto.quantidade = quantidade;
+    produto.validade = validade;
+
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    db.collection("produtos").add(produto.toMap());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +36,28 @@ class AlertDialogCustomizado extends StatelessWidget {
               decoration: InputDecoration(
                 labelText: "Quantidade",
               ),
+              keyboardType: TextInputType.number,
             ),
             TextField(
               controller: controllerValidade,
               decoration: InputDecoration(
                 labelText: "Validade",
               ),
+              keyboardType: TextInputType.datetime,
             ),
           ],
         ),
       ),
       actions: [
         FlatButton(
-          child: Text("Adicionar"),
-          onPressed: () {},
-        ),
+            child: Text("Adicionar"),
+            onPressed: () {
+              adicionarProduto(
+                controllerNome.text,
+                controllerQuantidade.text,
+                controllerValidade.text,
+              );
+            }),
         FlatButton(
           child: Text("Cancelar"),
           onPressed: () {
