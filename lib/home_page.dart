8889f89e8d1d0f13e88cfd6932eddace60c3624e
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-12 11:54:09 -0300

    adicionando AlertDialog no menu Adicionar item

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teste_flutter_api/alert_dioalog.dart';
 import 'package:teste_flutter_api/produtos_cadastrados.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +15,7 @@ class _HomePageState extends State<HomePage> {
     List<Widget> telas = [
       null,
       null,
-      null,
+      AlertDialogCustomizado(),
       null,
       null,
     ];
