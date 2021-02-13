17cc97f3b4426d831c1347acabee791e7f2ed0d1
Welaine Dantas <dev.welainedantas@gmail.com>
2021-02-13 16:58:30 -0300

    criando widget floatingactionbutton.expanded

---
+++ lib/widget/floatingActionButton.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonCustomizado extends StatelessWidget {
+  Widget icon;
+  String text;
+  Color color;
+  Function onPressed;
+
+  FloatingActionButtonCustomizado({
+    this.icon,
+    this.text,
+    this.color,
+    this.onPressed,
+  })
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      icon: icon,
+      label: Text(
+        text,
+        style: TextStyle(
+          fontSize: 16,
+        ),
+      ),
+      backgroundColor: color,
+      heroTag: null,
+      onPressed: onPressed,
+
+    );
+  }
+}
